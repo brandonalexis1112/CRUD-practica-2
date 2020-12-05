af18b4a9b6bfa09a22e3d83864f0eaeb4b79659f
BrandonAlexis <brandonalexis1112@gmail.com>
2020-12-04 20:25:42 -0500

    Septimo commit Vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,21 @@
-<template>  
-  <div >
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+<template>
+  <div id="app">
+    <div id="nav">
+     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-container>
          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
           <b-navbar-brand href="#">Brandon Alexis</b-navbar-brand>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-        <b-nav-item :to="{name: 'Skills'}" exact>Skills</b-nav-item>
-        <b-nav-item :to="{name: 'Resume'}">Resume</b-nav-item>
+        <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+        <b-nav-item :to="{name: 'About'}">About</b-nav-item>
       </b-navbar-nav>
         </b-collapse>
-
       </b-container>
       </b-navbar>
-    <div id="header">
-      <Search v-on:query-change="querySearch"/>
     </div>
-    <div id="main-container">
-      <h1>Skills</h1>
-      <TodoAdd v-on:add-todo="addTodo"/>
-      <Todos v-bind:todoslist="copyTodos" @delete-todo="deleteTodo" />
-    </div>
-
+    <router-view/>
   </div>
 </template>
 
-<script>
-//import HelloWorld from './components/HelloWorld.vue'
-import Search from './components/Search';
-import Todos from './components/Todos';
-import TodoAdd from './components/TodoAdd';
-
-export default {
-  name: 'App',
-  components: {
-     Todos,TodoAdd, Search
-  },
-  methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id != id);
-      this.copyTodos = [...this.todos]
-    },
-    addTodo(todo){
-      this.todos.push(todo);
-      this.copyTodos = [...this.todos];
-    },
-    querySearch(query){
-      if(query.trim() == ''){
-        this.copyTodos = [...this.todos];
-      }else{
-        const temp = this.todos.filter(todo =>{
-          return todo.title.includes(query)
-        });
-        this.copyTodos = [...temp];
-      }
-
-    }
-  },
-  data(){
-    return {
-      todos: [
-        {
-          id:0,
-          title: 'Vue',
-          completed: false
-        },
-          {
-          id:1,
-          title: 'JavaScript',
-          completed: true
-        },
-          {
-          id:2,
-          title: 'Html',
-          completed: false
-        },
-          {
-          id:3,
-          title: 'Css',
-          completed: true
-        }
-
-      ],
-      copyTodos: []
-    }
-  },
-  created() {
-    this.copyTodos = [...this.todos];
-  }
-}
-</script>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,95 @@
+<template>
+  
+  <div id="header">
+      <Search v-on:query-change="querySearch"/>
+      <div id="main-container">
+      <h1>Skills</h1>
+      <TodoAdd v-on:add-todo="addTodo"/>
+      <Todos v-bind:todoslist="copyTodos" @delete-todo="deleteTodo" />
+    </div>
+    </div>
+  
+
+   
+
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Search from '../components/Search';
+import Todos from '../components/Todos';
+import TodoAdd from '../components/TodoAdd';
+
+export default {
+  name: 'Home',
+  components: {
+    
+  Todos,TodoAdd,Search
+
+  },
+    methods: {
+      deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id);
+      this.copyTodos = [...this.todos]
+    },
+    
+    addTodo(todo){
+      this.todos.push(todo);
+      this.copyTodos = [...this.todos];
+    },
+    querySearch(query){
+      if(query.trim() == ''){
+        this.copyTodos = [...this.todos];
+      }else{
+        const temp = this.todos.filter(todo =>{
+          return todo.title.includes(query)
+        });
+        this.copyTodos = [...temp];
+      }
+
+    }
+  },
+      
+  data(){
+    return {
+      todos: [
+        {
+          id:0,
+          title: 'Vue',
+          completed: false
+        },
+          {
+          id:1,
+          title: 'JavaScript',
+          completed: true
+        },
+          {
+          id:2,
+          title: 'Html',
+          completed: false
+        },
+          {
+          id:3,
+          title: 'Css',
+          completed: true
+        }
+
+      ],
+      copyTodos: []
+    }
+  },
+  
+  created() {
+    this.copyTodos = [...this.todos];
+  }
+ 
+    
+    }
+
+
+  
+    
+  
+
+</script>
